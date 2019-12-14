12fd99fd5ad3c96e9767bbc03d72bbaedbcca633
kylejb <47502769+kylejb@users.noreply.github.com>
2019-12-14 01:06:33 -0500

    Initial Commit to Master

---
+++ ch_02/ex_02_01.py
@@ -0,0 +1,7 @@
+## Assignment | Chapter 02, Module 01
+#
+# PART A - Write a print statement, run the script through terminal, and take screenshot (see "Screenshot-ex_02_01_a)
+##
+# PART B - Take screenshot of your code within the text editor (Screenshot-ex_02_01_b)
+
+print("Hello Coursera")
