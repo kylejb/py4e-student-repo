014a8d9a4f6d08d37168c46d2734ee785cf5f590
kylejb <47502769+kylejb@users.noreply.github.com>
2020-01-01 01:04:02 -0500

    minor edit to comment (line1)

--- 3_Using-Python-to-Access-Web-Data/ch_13/ex_13_01.py
+++ 3_Using-Python-to-Access-Web-Data/ch_13/ex_13_01.py
@@ -1,7 +1,5 @@
-## Chapter 13 | Exercise 01
+## Chapter 13 | Exercise 01: Extracting Data from XML
 """
- Extracting Data from XML
-
 In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/geoxml.py. The program will prompt for a URL, read 
 the XML data from that URL using urllib and then parse and extract the comment counts from the XML data, compute the sum of the numbers in the file.
 
