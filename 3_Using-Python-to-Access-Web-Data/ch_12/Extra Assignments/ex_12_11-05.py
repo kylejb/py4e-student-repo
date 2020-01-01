1947c2cbef0921f56459e229f38d1c838f48406d
kylejb <47502769+kylejb@users.noreply.github.com>
2019-12-31 19:04:17 -0500

    updating assignment (ch12, ex05) with my solution

--- 3_Using-Python-to-Access-Web-Data/ch_12/Extra Assignments/ex_12_11-05.py
+++ 3_Using-Python-to-Access-Web-Data/ch_12/Extra Assignments/ex_12_11-05.py
@@ -11,14 +11,16 @@ mysock.connect(("data.pr4e.org", 80))
 cmd = "GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n".encode()
 mysock.send(cmd)
 
+dataList = list()
 while True:
     data = mysock.recv(512)
     if len(data) < 1:
         break
-    data = data.decode()
-    x = data.find("\r\n\r\n")
-    # issue: program does not finishing printing "data" from url... stops short, but it does seem to correctly skip headers.
-    print(data[x:])
-    # print(data.decode(), end="")
+    dataList.append(data.decode().strip())
 
 mysock.close()
+
+# Attempting to fix '/n' inconsistency issues; the space between quotes seems to matter when priting dataList, but not body. Why?
+dataList = " ".join(dataList)
+body = dataList.partition("\r\n\r\n")[2]
+print(body)
